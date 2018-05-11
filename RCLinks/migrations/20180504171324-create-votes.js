32f7ee72a0c5b53c720463018f2fc24ac8b577c8
Rafael Cardoso <rafaelalbuquerquecardoso@gmail.com>
2018-05-11 01:07:28 -0300

    Adicionando migrations

--- RCLinks/migrations/20180504171324-create-votes.js
+++ RCLinks/migrations/20180504171324-create-votes.js
@@ -1,29 +1,30 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Votes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      news_id: {
-        type: Sequelize.INTEGER
-      },
-      direction_vote: {
-        type: Sequelize.STRING
-      },
-      ip: {
-        type: Sequelize.STRING
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Votes');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Votes', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            news_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            direction_vote: {
+                type: Sequelize.STRING
+            },
+            ip: {
+                type: Sequelize.STRING
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Votes');
+    }
 };
--- RCLinks/migrations/20180504171444-create-blocked.js
+++ RCLinks/migrations/20180504171444-create-blocked.js
@@ -1,22 +1,22 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Blockeds', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      ip: {
-        type: Sequelize.STRING
-      },
-      until: {
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Blockeds');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Blockeds', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            ip: {
+                type: Sequelize.STRING
+            },
+            until: {
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Blockeds');
+    }
 };
